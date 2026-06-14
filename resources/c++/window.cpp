e536b128813b45830d9c2dbe96964b467993dc28
Koketso <koketsogaowelwe2@gmail.com>
2026-06-14 18:02:00 +0200

    setting up the window

--- resources/c++/window.cpp
+++ resources/c++/window.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <glad/glad.h> // OpenGL function loader
+#include <GLFW/glfw3.h> // Window creation
 
 using namespace std;
 
 int main(){
+
+    // Initiaise GLFW
+    bool windowInit = glfwInit();
+
+    if (!windowInit){
+        cerr << "GLFW init failed!!\n";
+        return -1;
+    }
+
+    // CONFIG THE OPENGL CONTEXT
+
+    // The version of OpenGl we want to use (version 4.6)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    // uses moreden OpenGL (includes shaders, VAOs, VBOs)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create the window
     return 0;
 }
