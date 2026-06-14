527c93a5b70ef0abbcb323d4ebbba59e2784e443
Koketso <koketsogaowelwe2@gmail.com>
2026-06-14 13:29:48 +0200

    create CMakeLists to build the project

---
+++ build/tests/CMakeFiles/inputlag.dir/__/deps/getopt.c.obj.d
@@ -0,0 +1,14 @@
+tests/CMakeFiles/inputlag.dir/__/deps/getopt.c.obj: \
+ C:\Users\Koketso\Documents\C++\glfw-3.4\deps\getopt.c \
+ C:\Users\Koketso\Documents\C++\glfw-3.4\deps\getopt.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h
---
+++ build/tests/CMakeFiles/reopen.dir/reopen.c.obj.d
@@ -0,0 +1,39 @@
+tests/CMakeFiles/reopen.dir/reopen.c.obj: \
+ C:\Users\Koketso\Documents\C++\glfw-3.4\tests\reopen.c \
+ C:/Users/Koketso/Documents/C++/glfw-3.4/deps/glad/gl.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/stdlib.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/limits.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/syslimits.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/limits.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/malloc.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/mm_malloc.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/errno.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/Users/Koketso/Documents/C++/glfw-3.4/include/GLFW/glfw3.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/time.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sys/timeb.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/_timeval.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/pthread_time.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/pthread_compat.h \
+ C:/Users/Koketso/Documents/C++/glfw-3.4/deps/linmath.h \
+ C:/Program\ Files\ (x86)/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1/mingw64/x86_64-w64-mingw32/include/math.h
